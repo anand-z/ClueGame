6e7f7f5cc6a15926d5bd2262389a60ab73b17e3b
anand-z <70981993+anand-z@users.noreply.github.com>
2020-10-05 14:01:57 -0600

    Var3

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Authors: Anand Zorig and Chantal Wang - Section C
+
+Contains: TestBoard.java, TestBoardCell.java, BoardTestsExp.java
+
+Worked on together over Saros
--- src/test/BoardTestsExp.java
+++ src/test/BoardTestsExp.java
@@ -80,8 +80,69 @@ public class BoardTestsExp {
 		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
 	
+	@Test
+	public void testTargetsNarrow() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 2);
+		Set <TestBoardCell> targets = board.getTargets();
+		//test possible targets created within 2 spaces of cell 0,0
+		//within a narrow hallway
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
+	}
 	
+	@Test
+	public void testTargetsVoidSpace() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 2);
+		Set <TestBoardCell> targets = board.getTargets();
+		//test Possible targets created within 3 spaces of cell 0,0
+		//while there is an unused space (x) is present at cell 1,1
+		Assert.assertEquals(2, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
+	}
 	
+	@Test
+	public void testEmptyTopLeft() {
+		board = new TestBoard(null, 0);
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set <TestBoardCell> targets = board.getTargets();
+		//test possible targets created within 4 spaces of cell 0,0
+		//while the board is a 4x4 and all walkway (w) spaces
+		Assert.assertEquals(6, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 4)));
+		Assert.assertTrue(targets.contains(board.getCell(4, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
+	}
+		
+	@Test
+	public void testEmptyBottomRight() {
+		board = new TestBoard(null,0);
+		TestBoardCell cell = board.getCell(3,3);
+		board.calcTargets(cell, 6);
+		Set <TestBoardCell> targets = board.getTargets();
+		//test possible targets created within 6 spaces of cell 3,3
+		//while the board is a 4x4 and all walkway (w) spaces
+		Assert.assertEquals(11, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(3, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 1))); 
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(0, 0)));
+	}
 
 }
 
